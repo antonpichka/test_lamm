af05ecc5c8997a9fe81fc3896dc6c949880e78ff
antonpichka <antonpichka@gmail.com>
2025-01-14 19:38:52 +0200

    refactoring(lib/named_utility): Upd

--- lib/named_utility/algorithms_utility.dart
+++ lib/named_utility/algorithms_utility.dart
@@ -1,5 +1,10 @@
+import 'dart:math';
 import 'package:library_architecture_mvvm_modify/library_architecture_mvvm_modify.dart';
 import 'package:meta/meta.dart';
+import 'package:test_lamm/model/season/season.dart';
+import 'package:test_lamm/model/stats/stats.dart';
+import 'package:test_lamm/model/user/user.dart';
+import 'package:uuid/uuid.dart';
 
 @immutable
 final class AlgorithmsUtility {
@@ -11,6 +16,32 @@ final class AlgorithmsUtility {
       return 0;
     }
     return int.parse(input ?? "0");
+  }
+
+  static User getUserWhereGenerated() {
+    return User(Uuid().v1(), DateTime.fromMillisecondsSinceEpoch(1736516582), Random().nextInt(2) == 0 ? "Discord" : "Steam");
+  }
+
+  static String getStringWhereGeneratedFromStrTemplate(String strTemplate) {
+    final List<String> listString = List.generate(100, (index) => "$strTemplate${index + 1}");
+    return listString[Random().nextInt(100)];
+  }
+
+  static Stats getStatsFromUser(User user) {
+    final List<int> listGameMatchesWon = List.generate(50, (index) => index+1);
+    final List<int> listGameMatchesLost = List.generate(50, (index) => index+1);
+    final List<int> listPoints = List.generate(500, (index) => index+1);
+    return Stats(
+        Uuid().v1(),
+        user,
+        Season(
+            "1356b4d2-d287-11ef-9cd2-0242ac120002",
+            1,
+            DateTime.fromMillisecondsSinceEpoch(1736516582),
+            DateTime.fromMillisecondsSinceEpoch(1736516582)),
+        listGameMatchesWon[Random().nextInt(50)],
+        listGameMatchesLost[Random().nextInt(50)],
+        listPoints[Random().nextInt(500)]);
   }
 
   static void debugPrintWhereConsoleFromThisClassAndText(Object thisClass, String text) {
--- lib/named_utility/keys_temp_cache_provider_utility.dart
+++ lib/named_utility/keys_temp_cache_provider_utility.dart
@@ -6,6 +6,8 @@ final class KeysTempCacheProviderUtility {
   static const String token = "token";
   /* EnumRoutersUtility */
   static const String enumRoutersUtility = "enumRoutersUtility";
+  /* Stats */
+  static const String stats = "stats";
 
   const KeysTempCacheProviderUtility._();
 }
